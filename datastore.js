75e55ef04e87d7517b48e3b95cf37102cab937a5
John Joo <john@Johns-iMac.local>
2018-04-28 14:14:39 -0700

    :i initial commit

---
+++ datastore.js
@@ -0,0 +1,107 @@
+const STORE = [
+  {
+    text: "It's the truth, I just want to report the truth.  It'd be a nice change of pace.",
+    answers: [ 
+      {text: 'Patch Adams', isCorrect: false},
+      {text: 'The Terminator', isCorrect: false},
+      {text: 'Happy Feet', isCorrect: false},
+      {text: 'Good Morning Vietnam', isCorrect: true}
+    ]
+  },
+
+  { 
+    text: "Oh please, look at you.  Look at the way you're holding your glass.  Look at your pinky",
+    answers: [
+      {text: "Mrs. Doubtfire", isCorrect: false},
+      {text: "Happy Feet", isCorrect: false},
+      {text: "The Birdcage", isCorrect: true},
+      {text: "The Radio Man", isCorrect: false}
+    ]
+  },
+
+  { 
+    text: "This is the worst time of night for me.  Too late for yesterday, to early for tomorrow",
+    answers: [
+      {text: "Hook", isCorrect: false},
+      {text: "Insomnia", isCorrect: true},
+      {text: "Good Morning Vietnam", isCorrect: false},
+      {text: "The Radio Man", isCorrect: false}
+    ]
+  },
+
+  {
+    text: "I do voices.", 
+    answers: [
+      {text: "The Birdcage", isCorrect: false},
+      {text: "Happy Feet", isCorrect: false}, 
+      {text: "The Radio Man", isCorrect: false},
+      {text: "Mrs. Doubtfire", isCorrect: true}
+    ]
+  },
+
+  {
+    text: "I'm going 25 miles an hour, Jack.",
+    answers: [
+      {text: "Patch Adams", isCorrect: false},
+      {text: "Aladdin", isCorrect: false},
+      {text: "RV", isCorrect: false},
+      {text: "Father's Day", isCorrect: true},
+    ]
+  },
+
+
+
+  {
+    text: "You have any idea the power of a condemned man, do ya?",
+    answers: [
+      {text: "Death to Smoochy", isCorrect: true},
+      {text: "The Birdcage", isCorrect: false},
+      {text: "Good Will Hunting", isCorrect: true},
+      {text: "Insomnia", isCorrect: false},
+    ]
+  },
+
+  {
+    text: "Jack, give me your hand.  We're going home.",
+    answers: [
+      {text: "Patch Adams", isCorrect: false},
+      {text: "Hook", isCorrect: true},
+      {text: "What Dreams May Come", isCorrect: false},
+      {text: "Bicentennial Man", isCorrect: false},
+    ]
+  },
+
+  {
+    text: "But I think it's time for you to shut up now.  Shut up please?",
+    answers: [
+      {text: "Father's Day", isCorrect: false},
+      {text: "Mrs. Doubtfire", isCorrect: false},
+      {text: "Good Morning Vietnam", isCorrect: false},
+      {text: "The Fisher King", isCorrect: true},
+    ]
+  },
+
+  {
+    text: "Boy I screwed up.  I'm in dog heaven.",
+    answers: [
+      {text: "The Incubator", isCorrect: false},
+      {text: "Inspirator", isCorrect: false},
+      {text: "All Dogs Go to Heaven", isCorrect: false},
+      {text: "What Dreams May Come", isCorrect: true},
+    ]
+  },
+
+  {
+    text: "Then you've done your job, you've done your job.  It's time for your next adventure.",
+    answers: [
+      {text: "RV", isCorrect: false},
+      {text: "Death to Smoochy", isCorrect: false},
+      {text: "Night at the Museum", isCorrect: false},
+      {text: "Night at the Museum: Secret of the Tomb ", isCorrect: true},
+    ]
+  },
+ 
+]
+
+
+
---
+++ index.js
@@ -0,0 +1,218 @@
+let answers = []; 
+
+function calculateScore() { 
+  return answers.filter(function(answer){
+    return answer.isCorrect}).length; 
+}
+
+function displayInstructions() {
+  $('section.content').html(generateInstructions());
+}
+
+function displayResults() {
+  $('section.content').html(generateResults());
+}
+
+function displayScoreboard() {
+  $('section.scoreboard').html(generateScoreboard());
+}
+
+function displayQuestion(questionNumber) {
+  const question = STORE[questionNumber];
+  const htmlText = generateQuestion(question, questionNumber);
+  $('section.content').html(htmlText); 
+}
+
+function displayQuestion(questionNumber) {
+  const question = STORE[questionNumber];
+  const htmlText = generateQuestion(question, questionNumber);
+  $('section.content').html(htmlText); 
+}
+
+function displayFeedback(answer) {
+  $('.content').html(generateFeedback(answer))
+}
+
+function generateInstructions() {
+  return `
+    <div class="instructions" "js-quiz-instructions">
+      <p class="first-instruction">Welcome to the Ultimate Robin Williams Quiz</p>
+      <p class="second-instruction">Match the Rob-Dub quote you see with the movie it's from</p>
+      <button id="start-button">>click to start<</button>
+    </div>
+    `
+}
+
+function generateScoreboard() {
+  return ` 
+  <ul class="scoreboard-style">
+  <li>Question: <span class="questionNumber">${answers.length+1}</span>/${STORE.length}</li>
+  <li>Score: <span class="score">${calculateScore()}</span></li>
+  </ul>
+  `  
+}
+
+function generateResults() {
+  if (calculateScore()>=7) {
+    return `
+      <section id="final-page">
+        <p class="final-score">Final Score: ${calculateScore()} out of 10</p>
+        <p class="final-message">Congratulations, you are a certified obscure Robin Williams quote level 1 specialist!</p>
+        <img class="results-img" src="https://media1.giphy.com/media/5ns1wuK1Y3NKxkm3qO/giphy.gif">
+        <div></div>
+        <button id="reset-button" type="reset">Start Over</button>
+      </section>
+    `; 
+  }
+  else {
+    return `
+    <section id="final-page">
+      <p class="final-score">Final Score: ${calculateScore()} out of 10</p>
+      <p class="final-message">You have failed the test. But Robin is sympathetic to your shortcomings.</p>
+      <img class="results-img" src="https://media.giphy.com/media/Dvw2lJqlTuJmo/giphy.gif">
+      <div></div>
+      <button id="reset-button" type="reset">Start Over</button>
+    </section>
+    `; 
+  }
+}
+
+function generateQuestion(question, questionNumber) {
+  return `
+    <form id="quiz-form">
+    <fieldset class="quiz-box" class="col-3">
+      <p class="quiz-question-number">Question #${questionNumber+1}</p>
+      <legend class="question" class="col-6">"${question.text}"</legend>
+
+      <ul class="choices-outside" background=url"http://www.wallpapers13.com/wp-content/uploads/2017/12/Genie-character-from-the-cartoon-Aladdin-HD-Wallpaper-1920x1080.jpg">
+        ${question.answers.map(function(answer, answerNumber){
+          return `
+          <li class="choices-inside">
+          <input type="radio" name="moviename" id="ans-${answerNumber}" value="${answerNumber}" required><label for="ans-${answerNumber}">${answer.text}</label>
+          </li>
+          `
+        }).join("")
+        }
+      </ul>
+
+        <button id="submit-button" type="submit">Final Answer, Regis</button>
+        <button id="reset-button" type="reset">Start Over</button>
+      
+    </fieldset>
+    </form>
+  `}
+
+function generateFeedback(answer) { 
+  if (answers.length<=9) {
+  return answer.isCorrect? 
+    //when answer is right 
+  `
+  <div class="correct-alert">
+    <p class="alert-text">You are absolutely correct!  Robin would be proud of you.</p>
+    <br>
+    <img class="feedback-img"  src="https://media3.giphy.com/media/WSTXcFbR2ZQOY/giphy.gif" >
+    
+    <button id="next-button" type="button">Go to the next question!</button>
+    <button id="reset-button" type="reset">Start Over</button>
+  </div>`: 
+    //when answer is wrong
+  `<div class="incorrect-alert">
+    <p class="alert-text">You are wrong.  Robin is disappointed in you.
+    The correct answer is "${getCorrectAnswer(STORE[answers.length-1]).text}"</p>
+    <img src="https://media2.giphy.com/media/BgN8YjBN0ZQis/giphy.gif" class="feedback-img">
+    
+    <button id="next-button" type="button">Go to the next question!</button>
+    <button id="reset-button" type="reset">Start Over</button>
+  </div>
+  `
+  } 
+  else {
+      return answer.isCorrect? 
+        //when answer is right 
+      `
+      <div class="correct-alert">
+        <p class="alert-text">You are absolutely correct!  Robin would be proud of you.</p>
+        <br>
+        <img class="feedback-img"  src="https://media3.giphy.com/media/WSTXcFbR2ZQOY/giphy.gif" >
+        
+        <button id="next-button" type="button">See the results!</button>
+        <button id="reset-button" type="reset">Start Over</button>
+      </div>`: 
+        //when answer is wrong
+      `<div class="incorrect-alert">
+        <p class="alert-text">You are wrong.  Robin is disappointed in you.
+        The correct answer is "${getCorrectAnswer(STORE[answers.length-1]).text}"</p>
+        <img src="https://media2.giphy.com/media/BgN8YjBN0ZQis/giphy.gif" class="feedback-img">
+        
+        <button id="next-button" type="button">See the results!</button>
+        <button id="reset-button" type="reset">Start Over</button>
+      </div>
+      `
+  }
+}
+
+function nextQuestion() {
+  if (answers.length >= STORE.length){
+    displayResults(); 
+  }
+  else { 
+    displayQuestion(answers.length);
+    displayScoreboard();
+  }
+   
+} 
+
+function getCorrectAnswer(question) {
+  return question.answers.find(function(answer){
+    return answer.isCorrect});
+}
+
+function handleStartQuiz(event) {
+  $('.content').on('click', '#start-button', function(event){
+    nextQuestion();
+  });
+}
+
+function handleSubmit(event) {
+  $('.content').on('click', '#submit-button', function(event) {
+    event.preventDefault(); 
+  const currentChoice = $('input[name=moviename]:checked').val();
+  if (currentChoice||currentChoice===0) {
+    const currentQuestion = STORE[answers.length];
+    const currentAnswer = currentQuestion.answers[currentChoice];
+    answers.push(currentAnswer);
+    displayFeedback(currentAnswer);
+    }
+  }); 
+}
+   
+function handleNextQuestion() {
+  $('.content').on('click', '#next-button', function(event){
+    nextQuestion(); 
+  });
+}
+
+function handleRestart() {
+  $('.content').on('click', '#reset-button', function(event){
+    displayInstructions(); 
+    answers=[]; 
+  });
+}
+
+function setupUI () {
+displayInstructions();
+};
+
+function setupEventHandlers () {
+handleStartQuiz(); 
+handleSubmit(); 
+handleNextQuestion(); 
+handleRestart();
+}
+
+function loadQuiz() { 
+  setupEventHandlers(); 
+  setupUI(); 
+}
+
+$(loadQuiz); 
